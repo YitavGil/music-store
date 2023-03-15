b769e37932c8961030bcd178f077fdc8eb171c42
YitavGil <yitavga@yahoo.com>
2023-03-15 06:08:13 +0200

    cart badge and state setup

--- src/scenes/global/Navbar.jsx
+++ src/scenes/global/Navbar.jsx
@@ -8,17 +8,12 @@ import {
 } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
 
 const Navbar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const icons = [
-    <SearchOutlined />,
-    <PersonOutline />,
-    <ShoppingBagOutlined />,
-    <MenuOutlined />,
-  ];
+  const cart = useSelector((state) => state.cart.cart);
 
   return (
     <Box
@@ -58,13 +53,37 @@ const Navbar = () => {
           columnGap={"20px"}
           zIndex="2"
         >
-          {icons.map((icon) => {
-            return(
-              <IconButton sx={{ color: 'black'}}>
-                {icon}
-              </IconButton>
-            )
-          })}
+          <IconButton sx={{ color: "black" }}>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton sx={{ color: "black" }}>
+            <PersonOutline />
+          </IconButton>
+
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .MuiBadge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0 4px",
+                height: "14px",
+                minWidth: '13px'
+              }
+            }}
+          >
+            <IconButton
+              onClick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "black" }}
+            >
+              <ShoppingBagOutlined />
+            </IconButton>
+          </Badge>
+          <IconButton sx={{ color: "black" }}>
+            <MenuOutlined />
+          </IconButton>
         </Stack>
       </Box>
     </Box>
