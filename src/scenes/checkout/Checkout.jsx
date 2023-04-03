f7c1af45ce8e29f4426c06344636ea3bf0832c05
YitavGil <yitavga@yahoo.com>
2023-04-03 19:12:34 +0300

    started checkout page

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { Box, Button, Stepper, Step, StepLabel } from "@mui/material";
+import { Formik } from "formik";
+import { useState } from "react";
+import * as yup from "yup";
+import { shades } from "../../theme";
+import Payment from "./Payment";
+import Shipping from "./Shipping";
+import { loadStripe } from "@stripe/stripe-js";
+
+const initialValues = {
+  billing
+}
 
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const cart = useState((state) => state.cart.cart);
+  const isFirstStep = activeStep === 0;
+  const isSecondStep = activeStep === 1;
+
+  const handleFormSubmit = async (value, actions) => {
+    setActiveStep(activeStep + 1)
+  };
+
+  async function makePayment(value) {
+
+  };
+
   return (
-    <div>Checkout</div>
+    <Box width="80%" m="100px auto">
+      <Stepper activeStep={activeStep} sx={{ m:"20px 0" }}>
+        <Step>
+          <StepLabel>Billing</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Payment</StepLabel>
+        </Step>
+      </Stepper>
+      <Box>
+        <Formik
+          onSubmit={handleFormSubmit}
+          initialValues={initialValues}
+          validationSchema={}
+        >
+
+        </Formik>
+      </Box>
+    </Box>
   )
 }
 
