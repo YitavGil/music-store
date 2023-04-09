c148dabc4af8904c6b6711469140036384553171
YitavGil <yitavga@yahoo.com>
2023-04-09 06:32:18 +0300

    setup uip schema

--- src/scenes/checkout/Checkout.jsx
+++ src/scenes/checkout/Checkout.jsx
@@ -32,7 +32,51 @@ const initialValues = {
   phoneNumber: "",
 };
 
-const checkoutSchema = []
+const checkoutSchema = [
+  yup.object().shape({
+    billingAddress: yup.object().shape({
+      firstName: yup.string().required("required"),
+      lastName:  yup.string().required("required"),
+      country:  yup.string().required("required"),
+      street1:  yup.string().required("required"),
+      street2:  yup.string(),
+      city:  yup.string().required("required"),
+      zipCode:  yup.string().required("required"),
+    }),
+    shippingAddress: yup.object().shape({
+      isSameAddress: yup.boolean(),
+      firstName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+      lastName: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+      country: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+      street1:yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+      street2:  yup.string(),
+      city:yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+      zipCode: yup.string().when("isSameAddress", {
+        is: false,
+        then: yup.string().required("required")
+      }),
+    }),
+  }),
+  yup.object().shape({
+    email: yup.string().required("required"),
+    phoneNumber: yup.string().required("required"),
+  })
+]
 
 const Checkout = () => {
   const [activeStep, setActiveStep] = useState(0);
@@ -62,7 +106,7 @@ const Checkout = () => {
         <Formik
           onSubmit={handleFormSubmit}
           initialValues={initialValues}
-          validationSchema={}
+          validationSchema={checkoutSchema[activeStep]}
         >
 
         </Formik>
